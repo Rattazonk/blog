8c18f9be63853e206457c453cb55c6c778e168f0
Frederik R. Vosberg <frederik.vosberg@rattazonk.com>
2014-07-24 21:49:38 +0200

    [ADDED] pages author relation

--- ext_tables.php
+++ ext_tables.php
@@ -5,6 +5,13 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Slimblog');
 
+
+// extend page attributes
+\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
+
+
+// add category page doktype
+
 $categoryPageDoktype = '48';
 $GLOBALS['PAGES_TYPES'][$categoryPageDoktype] = array(
 	'type' => 'web',
@@ -19,3 +26,24 @@ $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = array(
 $GLOBALS['TCA']['pages_language_overlay']['columns']['doktype']['config']['items'][] = array_shift(array_values(
 	$GLOBALS['TCA']['pages']['columns']['doktype']['config']['items']
 ));
+
+$newColumns = array(
+	'tx_slimblog_author' => array(
+		'exclude' => 0,
+		'label'	=> 'LLL:EXT:slimblog/Resources/Private/Language/locallang_db.xlf:author',
+		'config' => array(
+			'type' => 'select',
+			'size' => 10,
+			'foreign_table' => 'be_users',
+			'minitems' => 0,
+			'maxitems' => 999,
+		)
+	)
+);
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $newColumns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'tx_slimblog_author;;;;1-1-1');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay', $newColumns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay', 'tx_slimblog_author;;;;1-1-1');
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,9 @@
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
+	tx_slimblog_author int(11) DEFAULT '0' NOT NULL
+);
 
+CREATE TABLE pages_language_overlay (
+	tx_slimblog_author int(11) DEFAULT '0' NOT NULL
 );
